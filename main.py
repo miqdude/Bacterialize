747dda24f7e44ec66016ece061bca0d00d2410f3
miqdude <miqdadabdurhman@gmail.com>
2019-12-25 23:27:48 +0700

    adding sprite and resize image function

--- main.py
+++ main.py
@@ -13,23 +13,43 @@ class Position:
     self.x = 0
     self.y = 0
 
+class Sprite:
+  def __init__(self):
+    self.images = []
+    self.length = len(self.images)
+    self.resize = (0,0)
+  
+  # @param image_path : array or string
+  # set image to an object
+  def setImages(self, image_path):
+    if len(image_path) == 1:
+      self.images.append(pygame.image.load(image_path[0]))
+    else:
+      print("Not supported")
+
+  # draw sprite to screen
+  def draw(self, x, y):
+    if self.resize == (0,0):
+      win.blit(self.images[0], (x, y))
+    else:
+      # resize the image first
+      win.blit(pygame.transform.scale(self.images[0], self.resize), (x,y))
+
 class GameObject:
   def __init__(self):
     self.position = Position()
-    self.sprite = []
-    self.frameCount = 0
-    self.speed = 3
+    self.sprite = Sprite()
+    self.speed = 0
 
   def setPositionXY(self, x, y):
     self.position.x = x
     self.position.y = y  
 
   def addSprite(self, image_path):
-    self.sprite.append(pygame.image.load(image_path))
+    self.sprite.setImages(image_path)
 
   def draw(self):
-    if self.frameCount == 0 and self.sprite.__len__() == 1:
-      win.blit(self.sprite[0], (self.position.x, self.position.y))
+    self.sprite.draw(self.position.x, self.position.y)
 
   def move(self, direction):
     if (direction == 'UP'):
@@ -45,6 +65,7 @@ class GameObject:
 class Bacteria(GameObject):
   def __init__(self):
     super().__init__()
+    self.speed = 5
 
 class Egg(GameObject):
   def __init__(self):
@@ -52,10 +73,12 @@ class Egg(GameObject):
     self.isHatched = False
 
 player = Bacteria()
-player.addSprite('player.png')
+player.addSprite(['player.png'])
+player.sprite.resize = (100,100)
 
 egg = Egg()
-egg.addSprite('eggs.png')
+egg.addSprite(['eggs.png'])
+egg.sprite.resize = (50,50)
 
 run = True
 # main loop
